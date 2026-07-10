46846669b6538a24d2b5ffd7fc2ac8d08044f088
Kunal <20240802247@dypiu.ac.in>
2026-07-10 11:47:42 +0530

    cleared the tutorial endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from app.models import User
 app=FastAPI()
 
 @app.get("/")
 def root():
-    return{"message":"Hello World"}
+    return{
+        "message":"Ai Doc Search API"
+        }
+
+@app.post("/user")
+def create_user(user:User):
+    return{
+        "message":f"hello{user.name}",
+        "age":user.age
+    }
