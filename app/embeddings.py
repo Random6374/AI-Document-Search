4bf65f097ce79ab223cbe337c99fad582146b476
Kunal <20240802247@dypiu.ac.in>
2026-07-11 00:36:06 +0530

    embeddings done

--- app/embeddings.py
+++ app/embeddings.py
@@ -1,4 +1,14 @@
-from sentence_transformers import SentenceTransformer,util
+from sentence_transformers import SentenceTransformer
 model=SentenceTransformer("all-MiniLM-L6-v2")
 def create_embedding(text):
     return model.encode(text)
+
+def create_embeddings(chunks):
+    embeddings=[]
+    for chunk in chunks:
+        embedding=create_embedding(chunk)
+        embeddings.append({
+            "text":chunk,
+            "embedding":embedding
+        })
+    return embeddings
